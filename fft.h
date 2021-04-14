b0ac744dea51967d823bbce60906e0098b5e0211
Lloyd Rochester <lloyd.rochester@gmail.com>
2021-04-14 08:41:18 -0600

    fixed spelling typo

--- fft.h
+++ fft.h
@@ -13,7 +13,7 @@ fft(float data_re[], float data_im[], const unsigned int N);
 // helper functions called by the fft
 // data will first be rearranged then computed
 // an array of  {1, 2, 3, 4, 5, 6, 7, 8} will be
-// rarranged to {1, 5, 3, 7, 2, 6, 4, 8}
+// rearranged to {1, 5, 3, 7, 2, 6, 4, 8}
 void
 rearrange(float data_re[], float data_im[], const unsigned int N);
 
